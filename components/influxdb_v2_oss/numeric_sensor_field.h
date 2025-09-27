7672ab23a9dd8bccdb2708a5f9b79f5382bf5ef8
Simon Billemont <simon@angelcorp.be>
2025-09-27 09:16:32 +0200

    Cleanup some more format code

--- components/influxdb_v2_oss/numeric_sensor_field.h
+++ components/influxdb_v2_oss/numeric_sensor_field.h
@@ -8,21 +8,9 @@
 namespace esphome {
 namespace influxdb {
 
-enum class NumericSensorFieldFormat { Float, Integer, UnsignedInteger };
-
 class NumericSensorField : public Field {
 public:
   void set_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
-  void set_format(std::string format) {
-    if (format[0] == 'f') {
-      this->format_ = NumericSensorFieldFormat::Float;
-    } else if (format[0] == 'i') {
-      this->format_ = NumericSensorFieldFormat::Integer;
-    } else {
-      this->format_ = NumericSensorFieldFormat::UnsignedInteger;
-    }
-  }
-  void set_accuracy_decimals(int8_t val) { this->accuracy_decimals_ = val; }
 
   void do_setup() override;
   bool sensor_has_state() const override { return this->sensor_->has_state(); }
@@ -32,8 +20,6 @@ public:
 
 protected:
   sensor::Sensor *sensor_;
-  NumericSensorFieldFormat format_;
-  int8_t accuracy_decimals_{4};
 };
 
 }  // namespace influxdb
